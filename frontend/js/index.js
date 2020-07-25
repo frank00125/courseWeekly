119b773b8b32aac8398a89d5a0784272e9ac26f5
蘇智煒 <suzhiwei@suzhiweis-MacBook-Air.local>
2020-07-25 14:32:25 +0800

    add addArticle page

---
+++ frontend/js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById('jump-link').addEventListener('click',function(){
+
+        //  var article=document.getElementsByName('')
+        // document.styleSheets.getElementById('article')
+    });
+});
