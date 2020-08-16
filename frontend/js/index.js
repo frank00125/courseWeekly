e48b91d37d54f775a56252d6350e379f18b0ab45
cocoon <cocoon972265@hotmail.com>
2020-08-16 15:41:28 +0800

    add articalDetail

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded",function(){
     document.getElementById('jump-link').addEventListener('click',function(){
-
-        //  var article=document.getElementsByName('')
-        // document.styleSheets.getElementById('article')
     });
 });
